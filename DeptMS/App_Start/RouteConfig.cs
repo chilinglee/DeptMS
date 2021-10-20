b96d9d5eae2ec2356d80ae15b8665e322d6beb4a
Chi-Ling-Lee <19940818jillian@gmail.com>
2021-10-21 07:43:01 +0800

    調整起始頁 刪除Home controller

--- DeptMS/App_Start/RouteConfig.cs
+++ DeptMS/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DeptMS
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Department", action = "Read", id = UrlParameter.Optional }
             );
 
 
